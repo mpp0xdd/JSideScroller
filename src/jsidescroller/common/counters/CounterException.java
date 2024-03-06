7c2c420876a740078b0df556d5639859c596b586
mpp <mpp0xdd@gmail.com>
2024-03-06 22:55:39 +0900

    WIP

--- src/jsidescroller/common/counters/CounterException.java
+++ src/jsidescroller/common/counters/CounterException.java
@@ -7,7 +7,7 @@ abstract class CounterException extends Exception {
   private final Counter<?> counter;
   private final Number operand;
 
-  public CounterException(AbstractCounter<?, ?> counter, Number operand) {
+  public CounterException(Counter<?> counter, Number operand) {
     this.counter = Objects.requireNonNull(counter);
     this.operand = Objects.requireNonNull(operand);
   }
