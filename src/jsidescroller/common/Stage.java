532d663af21d6b04af90c1d0182576b71ce34438
mpp <mpp0xdd@gmail.com>
2024-03-10 18:42:30 +0900

    WIP

--- src/jsidescroller/common/Stage.java
+++ src/jsidescroller/common/Stage.java
@@ -23,67 +23,6 @@ import jsidescroller.common.interfaces.SideScrollerComponent;
 public abstract class Stage
     implements SubGameScreen, Drawable, Rectangular, Relocatable, GravitationalField {
 
-  public final class Viewport implements Rectangular, Locatable {
-
-    private final int x;
-    private final int y;
-
-    private Viewport() {
-      StageOffset offset = StageOffset.of(Stage.this);
-      this.x = offset.width();
-      this.y = offset.height();
-    }
-
-    public StagePoint first() {
-      Point location = getLocation();
-      return toStagePoint(location).orElseThrow();
-    }
-
-    public StagePoint last() {
-      Point location = new Point(x() + width() - 1, y() + height() - 1);
-      return toStagePoint(location).orElseThrow();
-    }
-
-    public Stream<StagePoint> stagePointStream() {
-      Stream.Builder<StagePoint> builder = Stream.builder();
-
-      StagePoint first = first();
-      StagePoint last = last();
-      for (int y = first.y(); y <= last.y(); y++) {
-        for (int x = first.x(); x <= last.x(); x++) {
-          builder.add(StagePoint.of(Stage.this, x, y));
-        }
-      }
-
-      return builder.build();
-    }
-
-    @Override
-    public int x() {
-      return x;
-    }
-
-    @Override
-    public int y() {
-      return y;
-    }
-
-    @Override
-    public Point getLocation() {
-      return new Point(x(), y());
-    }
-
-    @Override
-    public int width() {
-      return Stage.this.width();
-    }
-
-    @Override
-    public int height() {
-      return Stage.this.height();
-    }
-  }
-
   private final Point location;
 
   private final Map<StagePoint, Chip> stage;
@@ -302,6 +241,67 @@ public abstract class Stage
   private void drawSprite(Graphics g, Viewport viewport, Sprite sprite) {
     if (sprite.intersects(viewport)) {
       sprite.draw(g);
+    }
+  }
+
+  public final class Viewport implements Rectangular, Locatable {
+  
+    private final int x;
+    private final int y;
+  
+    private Viewport() {
+      StageOffset offset = StageOffset.of(Stage.this);
+      this.x = offset.width();
+      this.y = offset.height();
+    }
+  
+    public StagePoint first() {
+      Point location = getLocation();
+      return toStagePoint(location).orElseThrow();
+    }
+  
+    public StagePoint last() {
+      Point location = new Point(x() + width() - 1, y() + height() - 1);
+      return toStagePoint(location).orElseThrow();
+    }
+  
+    public Stream<StagePoint> stagePointStream() {
+      Stream.Builder<StagePoint> builder = Stream.builder();
+  
+      StagePoint first = first();
+      StagePoint last = last();
+      for (int y = first.y(); y <= last.y(); y++) {
+        for (int x = first.x(); x <= last.x(); x++) {
+          builder.add(StagePoint.of(Stage.this, x, y));
+        }
+      }
+  
+      return builder.build();
+    }
+  
+    @Override
+    public int x() {
+      return x;
+    }
+  
+    @Override
+    public int y() {
+      return y;
+    }
+  
+    @Override
+    public Point getLocation() {
+      return new Point(x(), y());
+    }
+  
+    @Override
+    public int width() {
+      return Stage.this.width();
+    }
+  
+    @Override
+    public int height() {
+      return Stage.this.height();
     }
   }
 }
