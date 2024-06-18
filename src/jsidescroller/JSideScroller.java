abb5c29a4a1ff4be6ba1dd19faf3ba29d7262b64
mpp <mpp0xdd@gmail.com>
2024-06-18 21:56:00 +0900

    WIP

--- src/jsidescroller/JSideScroller.java
+++ src/jsidescroller/JSideScroller.java
@@ -1,13 +1,19 @@
 package jsidescroller;
 
+import jglib.base.Game;
 import jglib.component.GameWindow;
 import jsidescroller.component.DefaultStage;
 import jsidescroller.component.DefaultStatusBar;
 import jsidescroller.screen.MainScreen;
 
-public class JSideScroller {
+public class JSideScroller extends Game {
 
   public static void main(String[] args) {
+    launch(JSideScroller.class);
+  }
+
+  @Override
+  protected void start() {
     GameWindow window = new GameWindow("JSideScroller");
     MainScreen screen = new MainScreen(new DefaultStatusBar(), new DefaultStage());
 
